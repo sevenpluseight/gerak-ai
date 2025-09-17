2ce133fb3d84dd19d24ce39e0dba62c487cad1c3
Carol Hew <112550947+sevenpluseight@users.noreply.github.com>
2025-09-17 22:51:56 +0800

    chore: update dependencies and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
+import { fileURLToPath } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +10,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "src"),
+    }
+  }
 })

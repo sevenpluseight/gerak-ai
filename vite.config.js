3fa3a2b726acad719fc8825b9e1789ff8e2ea9ca
Carol Hew <112550947+sevenpluseight@users.noreply.github.com>
2025-12-24 12:34:44 +0800

    feat: set base path for deployment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,31 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// import path from "path"
+// import { fileURLToPath } from 'url';
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "src"),
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/gerak-ai/',
   plugins: [
     react(),
     tailwindcss(),
